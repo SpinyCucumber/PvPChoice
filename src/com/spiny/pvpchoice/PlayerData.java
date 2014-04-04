7369fed5dc5ddcd13ab3e76e4109b38c247e3468
SpinyCucumber <daniel.hilty@gmail.com>
2014-04-03 21:54:43 -0500

    Added the relevant stuff.

---
+++ src/com/spiny/pvpchoice/PlayerData.java
@@ -0,0 +1,11 @@
+package com.spiny.pvpchoice;
+
+public class PlayerData {
+	
+	public boolean pvpEnabled;
+	public int cooldown = 0;
+	
+	public PlayerData(boolean pvpEnabled) {
+		this.pvpEnabled = pvpEnabled;
+	}
+}
